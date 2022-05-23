8d38f3ce5f2385ca5a9bbd3d2881e0d0edd4ba1b
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2022-05-23 09:31:42 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shohay/welcome.dart';
+import 'package:shohay/splashScreen/spash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MyApp(
+      child: MaterialApp(
+        title: 'Uber Clone',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: MySplashScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); //constructor
+class MyApp extends StatefulWidget {
+  final Widget? child;
+  const MyApp({this.child});
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Key key = UniqueKey();
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shohay',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: const Welcome(),
-    );
+    return KeyedSubtree(key: key, child: widget.child!);
   }
 }
